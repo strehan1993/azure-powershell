8f61eede9dd51059f8eeddceb9f8efbdcab77f7d
wyunchi-ms <yunwang@microsoft.com>
2020-09-08 12:28:22 +0800

    Update SDK version

--- src/Aks/Aks.Test/ScenarioTests/KubernetesTests.ps1
+++ src/Aks/Aks.Test/ScenarioTests/KubernetesTests.ps1
@@ -8,7 +8,7 @@ function Test-NewAzAksSimple
     $resourceGroupName = Get-RandomResourceGroupName
     $kubeClusterName = Get-RandomClusterName
     $location = Get-ProviderLocation "Microsoft.ContainerService/managedClusters"
-    $nodeVmSize = "Standard_A2"
+    $nodeVmSize = "Standard_D2_v2"
 
     try
     {
@@ -80,14 +80,14 @@ function Test-NewAzAks
     $kubeClusterName = Get-RandomClusterName
     $location = Get-ProviderLocation "Microsoft.ContainerService/managedClusters"
     $kubeVersion = "1.15.11"
-    $nodeVmSize = "Standard_A2"
+    $nodeVmSize = "Standard_D2_v2"
     $maxPodCount = 25
     $nodeName = "defnode"
     $nodeCount = 2
     $nodeMinCount = 1
     $nodeMaxCount = 30
     $nodeDiskSize = 100
-    $nodeVmSetType = "AvailabilitySet"
+    $nodeVmSetType = "VirtualMachineScaleSets"
     $nodeOsType = "Linux"
     $enableAutoScaling = $true
     $enableRbac = $true
@@ -96,31 +96,23 @@ function Test-NewAzAks
     $linuxAdminUser = "linuxuser"
     $dnsNamePrefix = "mypre"
     $updatedKubeVersion = "1.15.12"
-    $nodePoolName = 'system'
 
     try
     {
         New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-        # New-AzAks -ResourceGroupName $resourceGroupName -Name $kubeClusterName -NetworkPlugin $networkPlugin `
-        #     -KubernetesVersion $kubeVersion -EnableRbac -LoadBalancerSku $loadBalancerSku -LinuxProfileAdminUserName $linuxAdminUser -DnsNamePrefix $dnsNamePrefix `
-        #     -NodeName $nodeName -NodeOsType $nodeOsType -NodeCount $nodeCount -NodeOsDiskSize $nodeDiskSize
-        New-AzAks -ResourceGroupName $resourceGroupName -Name $kubeClusterName -NetworkPlugin $networkPlugin `
-            -KubernetesVersion $kubeVersion -EnableRbac -LoadBalancerSku $loadBalancerSku -LinuxProfileAdminUserName $linuxAdminUser -DnsNamePrefix $dnsNamePrefix
-        New-AzAksNodePool -ResourceGroupName $resourceGroupName -ClusterName $kubeClusterName -Name $nodePoolName -OsType $nodeOsType -Count $nodeCount -OsDiskSize $nodeDiskSize -VmSize $nodeVmSize `
-            -MaxCount $nodeMaxCount -MinCount $nodeMinCount -MaxPodCount $maxPodCount -NodeSetPriority Regular -EnableAutoScaling Deallocate -VmSetType VirtualMachineScaleSets
-        # if (IsLive) {
-        #     $cred = $(createTestCredential "Unicorns" "Puppies")
-        #     New-AzAks -ResourceGroupName $resourceGroupName -Name $kubeClusterName -ClientIdAndSecret $cred -NetworkPlugin $networkPlugin `
-        #         -KubernetesVersion $kubeVersion -EnableRbac -LoadBalancerSku $loadBalancerSku -LinuxProfileAdminUserName $linuxAdminUser -DnsNamePrefix $dnsNamePrefix `
-        #         -NodeName $nodeName -NodeOsType $nodeOsType -EnableNodeAutoScaling -NodeCount $nodeCount -NodeOsDiskSize $nodeDiskSize -NodeVmSize $nodeVmSize `
-        #         -NodeMaxCount $nodeMaxCount -NodeMinCount $nodeMinCount -NodeMaxPodCount $maxPodCount -NodeSetPriority Regular -NodeScaleSetEvictionPolicy Deallocate -NodeVmSetType VirtualMachineScaleSets
-        #  } else {
-        #     New-AzAks -ResourceGroupName $resourceGroupName -Name $kubeClusterName -NetworkPlugin $networkPlugin `
-        #         -KubernetesVersion $kubeVersion -EnableRbac -LoadBalancerSku $loadBalancerSku -LinuxProfileAdminUserName $linuxAdminUser -DnsNamePrefix $dnsNamePrefix `
-        #         -NodeName $nodeName -NodeOsType $nodeOsType -EnableNodeAutoScaling -NodeCount $nodeCount -NodeOsDiskSize $nodeDiskSize -NodeVmSize $nodeVmSize `
-        #         -NodeMaxCount $nodeMaxCount -NodeMinCount $nodeMinCount -NodeMaxPodCount $maxPodCount -NodeSetPriority Regular -NodeScaleSetEvictionPolicy Deallocate -NodeVmSetType VirtualMachineScaleSets
-        # }
+        if (IsLive) {
+            $cred = $(createTestCredential "Unicorns" "Puppies")
+            New-AzAks -ResourceGroupName $resourceGroupName -Name $kubeClusterName -NetworkPlugin $networkPlugin `
+                -KubernetesVersion $kubeVersion -EnableRbac -LoadBalancerSku $loadBalancerSku -LinuxProfileAdminUserName $linuxAdminUser -DnsNamePrefix $dnsNamePrefix `
+                -NodeName $nodeName -NodeOsType $nodeOsType -EnableNodeAutoScaling -NodeCount $nodeCount -NodeOsDiskSize $nodeDiskSize -NodeVmSize $nodeVmSize `
+                -NodeMaxCount $nodeMaxCount -NodeMinCount $nodeMinCount -NodeMaxPodCount $maxPodCount -NodeSetPriority Regular -NodeScaleSetEvictionPolicy Deallocate -NodeVmSetType VirtualMachineScaleSets
+         } else {
+            New-AzAks -ResourceGroupName $resourceGroupName -Name $kubeClusterName -NetworkPlugin $networkPlugin `
+                -KubernetesVersion $kubeVersion -EnableRbac -LoadBalancerSku $loadBalancerSku -LinuxProfileAdminUserName $linuxAdminUser -DnsNamePrefix $dnsNamePrefix `
+                -NodeName $nodeName -NodeOsType $nodeOsType -EnableNodeAutoScaling -NodeCount $nodeCount -NodeOsDiskSize $nodeDiskSize -NodeVmSize $nodeVmSize `
+                -NodeMaxCount $nodeMaxCount -NodeMinCount $nodeMinCount -NodeMaxPodCount $maxPodCount -NodeSetPriority Regular -NodeScaleSetEvictionPolicy Deallocate -NodeVmSetType VirtualMachineScaleSets
+        }
         $cluster = Get-AzAks -ResourceGroupName $resourceGroupName -Name $kubeClusterName
         Assert-NotNull $cluster.Fqdn
         Assert-AreEqual $dnsNamePrefix $cluster.DnsPrefix
@@ -129,16 +121,16 @@ function Test-NewAzAks
         Assert-AreEqual $networkPlugin $cluster.NetworkProfile.NetworkPlugin
         Assert-AreEqual $loadBalancerSku $cluster.NetworkProfile.LoadBalancerSku
         Assert-AreEqual $linuxAdminUser $cluster.LinuxProfile.AdminUsername
-        # Assert-AreEqual $nodeName $cluster.AgentPoolProfiles[0].Name
-        # Assert-AreEqual $nodeVmSize $cluster.AgentPoolProfiles[0].VmSize
-        # Assert-AreEqual $maxPodCount $cluster.AgentPoolProfiles[0].MaxPods
-        # Assert-AreEqual $nodeCount $cluster.AgentPoolProfiles[0].Count
-        # Assert-AreEqual $nodeMinCount $cluster.AgentPoolProfiles[0].MinCount
-        # Assert-AreEqual $nodeMaxCount $cluster.AgentPoolProfiles[0].MaxCount
-        # Assert-AreEqual $nodeDiskSize $cluster.AgentPoolProfiles[0].OsDiskSizeGB
-        # Assert-AreEqual $nodeVmSetType $cluster.AgentPoolProfiles[0].Type
-        # Assert-AreEqual $nodeOsType $cluster.AgentPoolProfiles[0].OsType
-        # Assert-AreEqual $enableAutoScaling $cluster.AgentPoolProfiles[0].EnableAutoScaling
+        Assert-AreEqual $nodeName $cluster.AgentPoolProfiles[0].Name
+        Assert-AreEqual $nodeVmSize $cluster.AgentPoolProfiles[0].VmSize
+        Assert-AreEqual $maxPodCount $cluster.AgentPoolProfiles[0].MaxPods
+        Assert-AreEqual $nodeCount $cluster.AgentPoolProfiles[0].Count
+        Assert-AreEqual $nodeMinCount $cluster.AgentPoolProfiles[0].MinCount
+        Assert-AreEqual $nodeMaxCount $cluster.AgentPoolProfiles[0].MaxCount
+        Assert-AreEqual $nodeDiskSize $cluster.AgentPoolProfiles[0].OsDiskSizeGB
+        Assert-AreEqual $nodeVmSetType $cluster.AgentPoolProfiles[0].Type
+        Assert-AreEqual $nodeOsType $cluster.AgentPoolProfiles[0].OsType
+        Assert-AreEqual $enableAutoScaling $cluster.AgentPoolProfiles[0].EnableAutoScaling
 
         Assert-AreEqual 1 $cluster.AgentPoolProfiles.Length
         $cluster = $cluster | Set-AzAks -NodeName $nodeName -NodeMinCount 2 -NodeMaxCount 28 -KubernetesVersion $updatedKubeVersion
